93000bde7452bf1959ba50ee8478b0efa33413ce
anreh <anurehka@gmail.com>
2019-11-06 11:54:22 +0800

    hi

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -8,30 +8,36 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
 
         val doneBtn: Button = findViewById(R.id.done_button)
 
-        doneBtn.setOnClickListener{ buttonCalled(it)}
+        binding.doneButton.setOnClickListener{ buttonCalled(it)}
 
     }
 
     private fun buttonCalled(view: View)
     {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        val editText = binding.nicknameEdit//findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = binding.nicknameText//findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
+        binding.nicknameText.text = binding.nicknameEdit.text
 
         view.visibility = View.GONE
-        editText.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.nicknameEdit.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
 
 
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
